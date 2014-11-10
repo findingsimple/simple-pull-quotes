1cec86672664c0c27251b53b0931890646e1912f
Jason Conroy <jason@findingsimple.com>
2014-11-10 11:34:18 +1100

    Spacing fix to use tabs, autop adjustment for better html

--- simple-pull-quotes.php
+++ simple-pull-quotes.php
@@ -34,205 +34,232 @@ if ( ! class_exists( 'Simple_Pull_Quotes' ) ) {
  */
 class Simple_Pull_Quotes {
 
-    private static $pullquote_fields;
-    
-    /**
-     * Initialise
-     */
-    function Simple_Pull_Quotes() {
+	private static $pullquote_fields;
+	
+	/**
+	 * Initialise
+	 */
+	function Simple_Pull_Quotes() {
 
-        self::$pullquote_fields = array(
-            'align' => __( 'Alignment' ),
-            'back' => __( 'Back' ),
-            'forward' => __( 'Forward' ),
-            'width' => __( 'Width' ),
-            'wrap' => __( 'Wrap' ),
-            'cite' => __( 'Citation')
-        );
-        
-        add_action( 'init', array( $this , 'simple_pull_quotes_init' ) );
-               
-        add_shortcode( 'pullquote', array( $this, 'pullquote' ) );
+		self::$pullquote_fields = array(
+			'align' => __( 'Alignment' ),
+			'back' => __( 'Back' ),
+			'forward' => __( 'Forward' ),
+			'width' => __( 'Width' ),
+			'wrap' => __( 'Wrap' ),
+			'cite' => __( 'Citation')
+		);
+		
+		add_action( 'init', array( $this , 'simple_pull_quotes_init' ) );
+			   
+		add_shortcode( 'pullquote', array( $this, 'pullquote' ) );
 
-    }
+	}
 
-    /**
-     * Apply appropriate hooks and filters
-     */
-    function simple_pull_quotes_init() {
+	/**
+	 * Apply appropriate hooks and filters
+	 */
+	function simple_pull_quotes_init() {
 
-    	if ( !is_admin() ) {
+		if ( !is_admin() ) {
 
-        	add_action( 'wp_enqueue_scripts', array( $this, 'add_simple_pull_quotes_script' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'add_simple_pull_quotes_script' ) );
 
-        }
+			add_filter( 'the_content', array( $this, 'shortcode_empty_paragraph_fix' ) );
 
-        if ( ( current_user_can('edit_posts') || current_user_can('edit_pages') ) && get_user_option('rich_editing') ) {
-          
-            add_action( 'admin_enqueue_scripts', array( $this, 'add_simple_pull_quotes_admin_scripts' ) );
+		}
+
+		if ( ( current_user_can('edit_posts') || current_user_can('edit_pages') ) && get_user_option('rich_editing') ) {
+		  
+			add_action( 'admin_enqueue_scripts', array( $this, 'add_simple_pull_quotes_admin_scripts' ) );
  
-            add_action( 'admin_footer', array( $this, 'the_jquery_dialog_markup' ) );
+			add_action( 'admin_footer', array( $this, 'the_jquery_dialog_markup' ) );
 
-            add_filter( 'mce_external_plugins', array( $this, 'mce_external_plugins' ) );
-        
-            add_filter( 'mce_buttons', array( $this, 'mce_buttons' ) );
-        
-        }
-        
-    }  
+			add_filter( 'mce_external_plugins', array( $this, 'mce_external_plugins' ) );
+		
+			add_filter( 'mce_buttons', array( $this, 'mce_buttons' ) );
+		
+		}
+		
+	}  
 
-    /**
-     * Add tinymce plugin
-     */
-    function mce_external_plugins( $plugin_array ) {
+	/**
+	 * Add tinymce plugin
+	 */
+	function mce_external_plugins( $plugin_array ) {
 
-        $plugin_array['simple_pull_quotes'] = plugins_url( '/tinymce/editor_plugin.js', __FILE__ );
+		$plugin_array['simple_pull_quotes'] = plugins_url( '/tinymce/editor_plugin.js', __FILE__ );
 
-        return $plugin_array;
+		return $plugin_array;
 
-    }
+	}
 
-    /**
-     * Add pull quote button WordPress editor
-     */
-    function mce_buttons( $buttons ) {
+	/**
+	 * Add pull quote button WordPress editor
+	 */
+	function mce_buttons( $buttons ) {
 
-        if ( ! in_array( 'simple_pull_quotes', $buttons ) )
-            array_push( $buttons, 'simple_pull_quotes' );
+		if ( ! in_array( 'simple_pull_quotes', $buttons ) )
+			array_push( $buttons, 'simple_pull_quotes' );
 
-        return $buttons;
+		return $buttons;
 
-    }
+	}
 
-    /**
-     * Register the front end script for displaying when a pull quote is used
-     */
-    function add_simple_pull_quotes_script() {
+	/**
+	 * Register the front end script for displaying when a pull quote is used
+	 */
+	function add_simple_pull_quotes_script() {
 
-        wp_register_script( 'simple-pull-quotes', plugins_url( 'js/simple-pull-quotes.js', __FILE__ ), array( 'jquery' ), '1', true );
-    
-    }
+		wp_register_script( 'simple-pull-quotes', plugins_url( 'js/simple-pull-quotes.js', __FILE__ ), array( 'jquery' ), '1', true );
+	
+	}
 
-    /**
-     * Add admin scripts
-     */
-    function add_simple_pull_quotes_admin_scripts() {
+	/**
+	 * Add admin scripts
+	 */
+	function add_simple_pull_quotes_admin_scripts() {
  
-        global $pagenow;
+		global $pagenow;
 
-        if ( is_admin() && $pagenow == 'post-new.php' || $pagenow == 'post.php' ) {
+		if ( is_admin() && $pagenow == 'post-new.php' || $pagenow == 'post.php' ) {
 
-            /**
-             * Add dashboard stylesheets
-             */
-            wp_enqueue_style( 'wp-jquery-ui-dialog' );
+			/**
+			 * Add dashboard stylesheets
+			 */
+			wp_enqueue_style( 'wp-jquery-ui-dialog' );
 
-            /**
-             * Add admin scripts
-             */
-            wp_enqueue_script( 'simple-pull-quotes-admin', plugins_url( '/js/simple-pull-quotes-admin.js', __FILE__ ), array( 'jquery-ui-dialog', 'jquery-ui-tabs' ), '1', true );
+			/**
+			 * Add admin scripts
+			 */
+			wp_enqueue_script( 'simple-pull-quotes-admin', plugins_url( '/js/simple-pull-quotes-admin.js', __FILE__ ), array( 'jquery-ui-dialog', 'jquery-ui-tabs' ), '1', true );
 
-            wp_localize_script( 'simple-pull-quotes-admin', 'pullquoteFields', self::$pullquote_fields );
-        
-        }
+			wp_localize_script( 'simple-pull-quotes-admin', 'pullquoteFields', self::$pullquote_fields );
+		
+		}
 
-    }
+	}
 
-    /**
-     * Add pullquote shortcode
-     */
-    function pullquote( $attr, $content = '' ) {
+	/**
+	 * Add pullquote shortcode
+	 */
+	function pullquote( $attr, $content = '' ) {
 
-        /**
-         * Enqueue the front end script when pull quotes are used
-         */
-        wp_enqueue_script( 'simple-pull-quotes' );
+		/**
+		 * Enqueue the front end script when pull quotes are used
+		 */
+		wp_enqueue_script( 'simple-pull-quotes' );
 
-        $defaults = array(
-            'align'   => 'left',
-            'back'    => 0,
-            'forward' => 0,
-            'width'   => '',
-            'wrap'    => '',
-            'cite'    => ''
-        );
+		$defaults = array(
+			'align'   => 'left',
+			'back'    => 0,
+			'forward' => 0,
+			'width'   => '',
+			'wrap'    => '',
+			'cite'    => ''
+		);
 
-        $attr = shortcode_atts( $defaults, $attr );
+		$attr = shortcode_atts( $defaults, $attr );
 
-        $attr['align'] = strtolower( $attr['align'] );
+		$attr['align'] = strtolower( $attr['align'] );
 
-        if ( ! in_array( $attr['align'], array( 'left', 'right', '' ) ) )
-            $attr['align'] = 'left';
+		if ( ! in_array( $attr['align'], array( 'left', 'right', '' ) ) )
+			$attr['align'] = 'left';
 
-        if ( ! empty( $attr['align'] ) )
-            $attr['align'] = ' align' . $attr['align'];
+		if ( ! empty( $attr['align'] ) )
+			$attr['align'] = ' align' . $attr['align'];
 
-        $data = '';
+		$data = '';
 
-        if ( ! empty( $attr['back'] ) )
-            $data = ' data-back="' . absint( $attr['back'] ) . '"';
-        elseif ( ! empty( $attr['forward'] ) )
-            $data = ' data-forward="' . absint( $attr['forward'] ) . '"';
+		if ( ! empty( $attr['back'] ) )
+			$data = ' data-back="' . absint( $attr['back'] ) . '"';
+		elseif ( ! empty( $attr['forward'] ) )
+			$data = ' data-forward="' . absint( $attr['forward'] ) . '"';
 
-        if ( ! empty( $attr['width'] ) )
-            $data .= ' style="width:' . esc_attr( $attr['width'] ) . '"';
+		if ( ! empty( $attr['width'] ) )
+			$data .= ' style="width:' . esc_attr( $attr['width'] ) . '"';
 
-        if ( ! empty( $attr['wrap'] ) )
-            $data .= ' data-wrap="' . esc_attr( $attr['wrap'] ) . '"';
+		if ( ! empty( $attr['wrap'] ) )
+			$data .= ' data-wrap="' . esc_attr( $attr['wrap'] ) . '"';
 
-        if ( ! empty( $attr['cite'] ) )
-            $data .= ' data-cite="' . esc_attr( $attr['cite'] ) . '"';
+		if ( ! empty( $attr['cite'] ) )
+			$data .= ' data-cite="' . esc_attr( $attr['cite'] ) . '"';
 
-        return '<span class="pullquote' . $attr['align'] . '"' . $data . ' ' . $cite . '>'. do_shortcode( $content ) .'</span>';
+		return Simple_Pull_Quotes::remove_wpautop( '<span class="pullquote' . $attr['align'] . '"' . $data . ' ' . $cite . '>'. do_shortcode( $content ) .'</span>' );
  
-    }
+	}
 
-    /**
-     * Build jQuery UI Window.
-     */
-    function the_jquery_dialog_markup() {
+	/**
+	 * Build jQuery UI Window.
+	 */
+	function the_jquery_dialog_markup() {
 
-        $screen = get_current_screen();
+		$screen = get_current_screen();
 
-        if ( $screen->base != 'post' )
-                return;
-        ?>
+		if ( $screen->base != 'post' )
+				return;
+		?>
 <div class="hidden">
-    <div id="simple-pull-quotes-dialog" title="Insert Pull Quote">
-        <div id="pull-quote-details" class="clear clearfix" style="margin: 1em; clear:both">
-        <?php foreach ( self::$pullquote_fields as $field_id => $field_label ) { ?>
-            <?php if ( $field_id == 'align' ) { ?>
-            <div style="display: inline-block; width: 100%; margin: 2px;">
-            <label for="pull-quote-<?php echo $field_id; ?>" style="display: inline-block; width: 20%;">
-                <?php echo $field_label; ?></label>
-                <select id="pull-quote-<?php echo $field_id; ?>" name="pull-quote-<?php echo $field_id; ?>" style="width: 75%; float: right;" >
-                    <option></option>
-                    <option value="left">Left</option>
-                    <option value="right">Right</option>
-                </select> 
-            </div>
-           <?php } elseif ( $field_id == 'wrap' ) { ?>
-            <div style="display: inline-block; width: 100%; margin: 2px;">
-            <label for="pull-quote-<?php echo $field_id; ?>" style="display: inline-block; width: 20%;">
-                <?php echo $field_label; ?></label>
-                <select id="pull-quote-<?php echo $field_id; ?>" name="pull-quote-<?php echo $field_id; ?>" style="width: 75%; float: right;" >
-                    <option></option>
-                    <option value="true">True</option>
-                </select> 
-            </div>
-            <?php } else { ?>
-            <label for="pull-quote-<?php echo $field_id; ?>" style="display: inline-block; width: 100%; margin: 2px;">
-                <?php echo $field_label; ?>
-                <input type="text" id="pull-quote-<?php echo $field_id; ?>" name="pull-quote-<?php echo $field_id; ?>" value=""  style="width: 75%; float: right;"/>
-            </label>
-            <?php } ?>
-        <?php } ?>
-        </div><!-- #pull-quote-details -->
-    </div><!-- #simple-pull-quotes-dialog -->
+	<div id="simple-pull-quotes-dialog" title="Insert Pull Quote">
+		<div id="pull-quote-details" class="clear clearfix" style="margin: 1em; clear:both">
+		<?php foreach ( self::$pullquote_fields as $field_id => $field_label ) { ?>
+			<?php if ( $field_id == 'align' ) { ?>
+			<div style="display: inline-block; width: 100%; margin: 2px;">
+			<label for="pull-quote-<?php echo $field_id; ?>" style="display: inline-block; width: 20%;">
+				<?php echo $field_label; ?></label>
+				<select id="pull-quote-<?php echo $field_id; ?>" name="pull-quote-<?php echo $field_id; ?>" style="width: 75%; float: right;" >
+					<option></option>
+					<option value="left">Left</option>
+					<option value="right">Right</option>
+				</select> 
+			</div>
+		   <?php } elseif ( $field_id == 'wrap' ) { ?>
+			<div style="display: inline-block; width: 100%; margin: 2px;">
+			<label for="pull-quote-<?php echo $field_id; ?>" style="display: inline-block; width: 20%;">
+				<?php echo $field_label; ?></label>
+				<select id="pull-quote-<?php echo $field_id; ?>" name="pull-quote-<?php echo $field_id; ?>" style="width: 75%; float: right;" >
+					<option></option>
+					<option value="true">True</option>
+				</select> 
+			</div>
+			<?php } else { ?>
+			<label for="pull-quote-<?php echo $field_id; ?>" style="display: inline-block; width: 100%; margin: 2px;">
+				<?php echo $field_label; ?>
+				<input type="text" id="pull-quote-<?php echo $field_id; ?>" name="pull-quote-<?php echo $field_id; ?>" value=""  style="width: 75%; float: right;"/>
+			</label>
+			<?php } ?>
+		<?php } ?>
+		</div><!-- #pull-quote-details -->
+	</div><!-- #simple-pull-quotes-dialog -->
 </div><!-- .hidden -->
 <?php
-    }
+	}
 
+	/**
+	 * Replaces WP autop formatting 
+	 *
+	 */
+	function remove_wpautop($content) {
+		$content = do_shortcode( shortcode_unautop( $content ) ); 
+		$content = preg_replace( '#^<\/p>|^<br \/>|<p>$#', '', $content);
+		return $content;
+	}
+
+	/**
+	 * Shortcode Empty Paragraph Fix
+	 */
+	function shortcode_empty_paragraph_fix( $content ) {
+
+		$array = array (
+			'<p>[' => '[',
+			']</p>' => ']',
+			']<br />' => ']'
+		);
+
+		$content = strtr( $content, $array );
+
+		return $content;
+	}
 
 }
 
